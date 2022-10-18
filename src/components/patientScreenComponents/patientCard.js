724e61d8850c37f69bb407ed47e6772e51c70132
satyansh-y <56058777+satyansh-y@users.noreply.github.com>
2022-10-18 18:07:39 -0500

    Added Patient page and patient Card files

---
+++ src/components/patientScreenComponents/patientCard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PatientCard(props) {
+    return ( 
+        
+     );
+}
+
+export default PatientCard;
---
+++ src/screens/PatientScreen.js
@@ -0,0 +1,19 @@
+import React, {useEffect, useState } from "react";
+import {
+    SafeAreaView,
+    View,
+    Text,
+    StyleSheet,
+    StatusBar,
+    Platform,
+    FlatList,
+  } from "react-native";
+  import PatientCard from "../components/patientScreenComponents/patientCard";
+
+function PatientScreen(props) {
+    return ( 
+
+     );
+}
+
+export default PatientScreen;
