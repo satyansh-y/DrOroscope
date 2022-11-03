00b852c27a84052e478849af3ed9795e5ce0c6b1
satyansh-y <56058777+satyansh-y@users.noreply.github.com>
2022-11-03 17:57:24 -0500

    Old Patient screen and card

--- src/components/patientScreenComponents/patientCard.js
+++ src/components/patientScreenComponents/patientCard.js
@@ -1,13 +1,39 @@
-import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { TextInput } from "react-native-paper";
+import { StyleSheet } from "react-native";
+import { Card, Avatar } from "react-native-paper";
+import Icon from "react-native-vector-icons/Ionicons";
 
-function PatientCard(props) {
-    return ( 
-        <SafeAreaView>
-            <Text>Hello</Text>
-        </SafeAreaView>
-        
-     );
-}
+const PatientCard = (props) => {
+  return (
+    <Card style={styles.card} elevation={2} mode="outlined" onPress={props.onPress}>
+      <Card.Title
+        title={props.patientName}
+        subtitle={props.patientID}
+        titleStyle={styles.text}
+        subtitleStyle={styles.subtext}
+        left={(props) => <Icon name="person" style={styles.icon} />}
+      />
+    </Card>
+  );
+};
 
 export default PatientCard;
+const styles = StyleSheet.create({
+  card: {
+    
+    height: 74,
+    backgroundColor: "#cad6ec",
+    marginTop: 12
+  },
+  text: {
+    fontSize: 20,
+  },
+  subtext: {
+    fontSize: 15,
+    color: "black",
+    alignSelf: "flex-end",
+  },
+  icon: {
+    fontSize: 30,
+  },
+});
--- src/screens/PatientScreen.js
+++ src/screens/PatientScreen.js
@@ -1,20 +1,79 @@
-import React, {useEffect, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
+
 import {
-    SafeAreaView,
-    View,
-    Text,
-    StyleSheet,
-    StatusBar,
-    Platform,
-    FlatList,
-  } from "react-native";
-  import PatientCard from "../components/patientScreenComponents/patientCard";
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  StatusBar,
+  Platform,
+  FlatList,
+} from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
+import PatientCard from "../components/patientCard";
+import { Button, FAB } from "react-native-paper";
+
+
+
+
 
 function PatientScreen(props) {
-    return ( 
-      <PatientCard/>
+  const [patientList, updatePatients] = useState([]);
+  
 
-     );
+
+
+
+
+  return (
+    <SafeAreaView style={styles.layer}>
+      
+      <View style={styles.header}>
+        <Icon name="people" style={styles.peopleIcon} />
+        <Text style={styles.title}>Patients</Text>
+      </View>
+      {/* <SearchBar style={{ margin: 10, backgroundColor: "#cad6ec" }} /> */}
+
+      <FlatList
+        scrollEnabled={true}
+        data={patientList}
+        renderItem={({ item, index }) => (
+          <PatientCard
+            patientName={item.Name}
+            patientID={"ID: " + item.id.substring(1, 4)}
+            onPress={PatientInfo}
+          />
+        )}
+      />
+
+      <FAB icon="plus" style={styles.button} onPress={addPatient} />
+    </SafeAreaView>
+  );
 }
 
 export default PatientScreen;
+const styles = StyleSheet.create({
+  header: {
+    alignItems: "center",
+    backgroundColor: "#FFFFF7",
+  },
+
+  layer: {
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  title: {
+    fontSize: 20,
+  },
+  peopleIcon: {
+    fontSize: 40,
+  },
+  button: {
+    position: "absolute",
+    backgroundColor: "black",
+    bottom: 50,
+    right: 20,
+  },
+});
